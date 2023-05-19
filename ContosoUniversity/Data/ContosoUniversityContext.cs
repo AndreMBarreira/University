e1498dec22d5c77de0dc486130a6956e540437d4
AndreMBarreira <andremilla.am@gmail.com>
2023-05-19 00:06:25 -0300

    complex data model

--- ContosoUniversity/Data/ContosoUniversityContext.cs
+++ ContosoUniversity/Data/ContosoUniversityContext.cs
@@ -21,5 +21,25 @@ namespace ContosoUniversity.Data
         public DbSet<Course>? Course { get; set; }
 
         public DbSet<Instructor>? Instructor { get; set; }
+
+        public DbSet<Department> Department { get; set; }
+
+        public DbSet<OfficeAssignment> OfficeAssignment { get; set; }
+
+        public DbSet<CourseAssignment> CourseAssignment { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Department>().ToTable("Department");
+            modelBuilder.Entity<Instructor>().ToTable("Instructor");
+            modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignment");
+            modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment");
+
+            modelBuilder.Entity<CourseAssignment>()
+                .HasKey(c => new { c.CourseID, c.InstructorID });
+        }
     }
 }
